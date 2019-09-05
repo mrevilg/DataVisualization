dbfbe86ff03fa6a3765aaa656dbb7f2b92e5be89
Stephen Clements <mrevilg@gmail.com>
2019-09-05 05:51:44 -0500

    Direction Indication

--- rw_visual.py
+++ rw_visual.py
@@ -2,8 +2,18 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Make a random walk, and plot the points.
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+# Keep making random walks, as long as is active.
+while True:
+    # Make a random walk, and plot the points.
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+        edgecolor='none', s=15)
+    
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
