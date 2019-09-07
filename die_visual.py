713405508a2a93548801ed15b3864fd44bda6584
Stephen Clements <mrevilg@gmail.com>
2019-09-07 14:32:51 -0500

    Die Visual and Freq

---
+++ die_visual.py
@@ -0,0 +1,18 @@
+from die import Die
+
+# Create a D6
+die = Die()
+
+# Make some rolls, store the result in a list
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# Analye the results.
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
