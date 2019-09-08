6489451b8959f06cedf04e9265f567800ae28c54
Stephen Clements <mrevilg@gmail.com>
2019-09-08 13:08:20 -0500

    Die Bar Chart

--- die_visual.py
+++ die_visual.py
@@ -1,4 +1,5 @@
 from die import Die
+import pygal
 
 # Create a D6
 die = Die()
@@ -14,5 +15,18 @@ frequencies = []
 for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
+
+# Visualize the results.
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6' ]
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
+
+# Note - Open webbroswer and open SVG file, wherever located to see results
 
 print(frequencies)
