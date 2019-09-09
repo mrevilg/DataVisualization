0855c8a4b94251cad36127fab33d3cf220a88005
Stephen Clements <mrevilg@gmail.com>
2019-09-09 06:15:31 -0500

    D6 and D10 50k rolls

--- die_visual.py
+++ die_visual.py
@@ -1,30 +1,34 @@
 from die import Die
 import pygal
 
-# Create a D6
-die = Die()
+# Create two D6 dice.
+die_1 = Die()
+die_2 = Die(10)
+
 
 # Make some rolls, store the result in a list
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Analye the results.
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.
 hist = pygal.Bar()
 
-hist.title = "Results of rolling one D6 1000 times."
-hist.x_labels = ['1', '2', '3', '4', '5', '6' ]
+hist.title = "Results of rolling a D6 and a D10 50,000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
+    '13', '14', '15', '16' ]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6', frequencies)
+hist.add('D6 + D10', frequencies)
 hist.render_to_file('die_visual.svg')
 
 # Note - Open webbroswer and open SVG file, wherever located to see results
