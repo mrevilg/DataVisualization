a95a61850785e2c25215fa4251bda4f98bf7f9a8
Stephen Clements <mrevilg@gmail.com>
2019-09-17 06:15:23 -0500

    Full 2014 Data

--- highs_lows.py
+++ highs_lows.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 
 # Get dates and high temperatures from file.
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -28,7 +28,7 @@ fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c="red")
 
 # Format polt.
-plt.title("Daily high temeratures, July 2014", fontsize=24)
+plt.title("Daily high temeratures - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
