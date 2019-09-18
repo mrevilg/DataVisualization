f254536503f5dcdca6ba58807112a45d152d8802
Stephen Clements <mrevilg@gmail.com>
2019-09-18 06:30:24 -0500

    Highs and Lows

--- highs_lows.py
+++ highs_lows.py
@@ -4,19 +4,22 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 
-# Get dates and high temperatures from file.
+# Get date, high, and low temperatures from file.
 filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-    dates, highs = [], []
+    dates, highs, lows, = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
 
         high = int(row[1])
         highs.append(high)
+
+        low = int(row[3])
+        lows.append(low)
     
     print(highs)
 
@@ -26,9 +29,10 @@ with open(filename) as f:
 # Plot data.
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c="red")
+plt.plot(dates, lows, c='blue')
 
 # Format polt.
-plt.title("Daily high temeratures - 2014", fontsize=24)
+plt.title("Daily high and low temeratures - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
